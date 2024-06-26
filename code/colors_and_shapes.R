1316c8e2859ed0849f8b4c3d9980b6e544367d8b
Katie L. Duggan <kld93@cornell.edu>
2024-06-25 22:27:11 -0400

    worked on taxonomy plots

---
+++ code/colors_and_shapes.R
@@ -0,0 +1,49 @@
+# This file indicates that colors and shapes of metadata across various
+# let's keep the visualizations consistent! :) 
+
+#install.packages("randomcoloR")
+#library(randomcoloR)
+#n <- 40
+#palette <- distinctColorPalette(n)
+
+# Set the phylum colors
+phylum_colors <- c(
+  "Acidobacteria" = "navy",
+  "Actinobacteria" = "darkslategray2",
+  "Bacteroidetes" = "deeppink1",
+  "Chloroflexi" = "plum2",
+  "Proteobacteria" = "gold", 
+  "Unclassified/Unassigned" = "plum1",
+  "Verrucomicrobia" = "red1",
+  "Candidatus Rokubacteria" = "firebrick",
+  "Deinococcus-Thermus" = "black", 
+  "Firmicutes" = "limegreen",
+  "Candidate Division NC10" = "grey",
+  "Euryarchaeota" = "magenta",
+  "Planctomycetes" = "#3E9B96",
+  "Ascomycota" = "greenyellow",
+  "Candidatu's Handelsmanbacteria" = "yellow",
+  "Basidiomycota" = "#B5D6AA",
+  "Chlorophyta" = "palevioletred1",
+  "Chytridiomycota" = "royalblue",
+  "Ciliophora" = "darkorange",
+  "Euglenozoa" = "olivedrab",
+  "Foraminifera" = "greenyellow",
+  "Perkinsozoa" = "green",
+  "Apicomplexa" = "darkorchid1",
+  "Haptophyta" =  "#657CDC",
+  "Heterolobosea" =  "#A77D87",
+  "Microsporidia" =  "#55AD91",
+  "Mucoromycota" =  "#E04F5D",
+  "Bacillariophyta" =  "#D47FE7",
+  "Nitrospirae" = "#D7E63E",
+  "Oomycota" =  "#42404C",
+  "Parabasalia" =  "#9BA195",
+  "Evosea" =  "#B0A2E8",
+  "Rhodophyta" =  "#92AB76",
+  "Zoopagomycota" =  "#DBB46E",
+  "Cyanobacteria" =  "#E355A9",
+  "Cercozoa" =  "#A8E8EA",
+  "Fornicata" =  "#E3C2DD",
+  "Imbricatea" =  "#E38B80",
+  "Viruses" =  "#5ABF66")
